41c44d5ed4ce1c5ad5d459fcb3c59fc7040ccfce
pedernera <pederneragerman@gmail.com>
2026-06-23 21:55:37 -0300

    Auto-configurar Firebase en producción sin .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || atob("QUl6YVN5Q3UwbUJYNkZEZXE1VHhUMEtjQU1weFFKaHZNcC1aMkFn"),
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "tpperezdario.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "tpperezdario",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "tpperezdario.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1091719990057",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1091719990057:web:ab0e6e5d079fdb93df4538"
 };
 
 const app = initializeApp(firebaseConfig);
